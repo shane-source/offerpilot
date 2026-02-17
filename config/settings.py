4d5ca9dca7dd5e96bcd69e235429c0931023638c
Shane Mutisya <mutisyashane1@gmail.com>
2026-02-17 16:03:24 +0300

    Deploy setup (gunicorn + whitenoise + build.sh)

--- config/settings.py
+++ config/settings.py
@@ -27,6 +27,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
 ]
 
 ROOT_URLCONF = "config.urls"
@@ -69,6 +70,8 @@ USE_TZ = True
 
 STATIC_URL = "/static/"
 STATICFILES_DIRS = [BASE_DIR / "static"]
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media"
